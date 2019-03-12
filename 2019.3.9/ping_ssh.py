34388fd67aacfa56c6ecda46f935d0e9cafa4ec1
luckily18894 <185686792@qq.com>
2019-03-12 09:10:09 +0800

    2019.3.11 task-1

---
+++ 2019.3.9/ping_ssh.py
@@ -0,0 +1,26 @@
+import paramiko
+import logging
+logging.getLogger("kamene.runtime").setLevel(logging.ERROR)  # 关闭不必要的报错
+from kamene.all import *
+from ping_one import ping1
+from paramiko_ssh import py_ssh
+
+
+def ping_ssh(ipadd, usern, passw, cmd='ls'):
+    for a in ipadd:
+        if ping1(a) == '1':
+            res = py_ssh(a, username=usern, password=passw, cmd=cmd)
+            print('登陆'+a+'并'+cmd+'回显结果为：')
+            print(res)
+        else:
+            print(a+'不通')
+
+
+if __name__ == '__main__':
+    l1 = ['192.168.1.104', '192.168.1.106']
+    username = 'root'
+    password = 'luCKi1y18894'
+    ping_ssh(l1, username, password)
+
+
+
